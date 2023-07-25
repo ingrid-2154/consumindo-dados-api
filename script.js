50a87b8f22ea89c4fec87c54d1055d449b89f835
ingrid-2154 <78661887+ingrid-2154@users.noreply.github.com>
2023-07-25 16:04:50 +0000

    Mensagem de erro.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);    
         var consultaCEPConvetida = await consultaCEP.json();
@@ -15,9 +17,10 @@ async function buscaEndereco(cep) {
         estado.value = consultaCEPConvetida.uf;
         bairro.value = consultaCEPConvetida.bairro;
 
-        console.log(consultaCEPConvetida);
+       
         return consultaCEPConvetida;
     } catch (erro) {
+        mensagemErro.innerHTML =`<p> CEP inválido. Tente novamente!</p>`
         console.log(erro)
     }
 }
