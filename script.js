59a6cfbfae6d5240cfa4c1cefe4e77ab87e38dc1
ingrid-2154 <78661887+ingrid-2154@users.noreply.github.com>
2023-07-25 15:05:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+async function buscaEndereco(cep){
+    try{
+        var consultaCEP = await fetch(`https:viacep.com.br/ws/${cep}/json/`);    
+        var consultaCEPConvetida = await consultaCEP.json();
+        if(consultaCEPConvetida.erro){
+            throw Error('CEP não existente');
+        }
+        console.log(buscaEndereco)
+        return consultaCEPConvetida
+    } catch (erro) {
+        console.log(erro)
+    }
+}
+
+
