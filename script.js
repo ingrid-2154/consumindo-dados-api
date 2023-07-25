e383a27d93b4f0709d0ea86dfa56b7e048705677
ingrid-2154 <78661887+ingrid-2154@users.noreply.github.com>
2023-07-25 15:16:29 +0000

    .

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-async function buscaEndereco(cep){
-    try{
+async function buscaEndereco(cep) {
+    try {
         var consultaCEP = await fetch(`https:viacep.com.br/ws/${cep}/json/`);    
         var consultaCEPConvetida = await consultaCEP.json();
-        if(consultaCEPConvetida.erro){
-            throw Error('CEP não existente');
+        if (consultaCEPConvetida.erro) {
+            throw Error('CEP não existente!');
         }
-        console.log(buscaEndereco)
-        return consultaCEPConvetida
+        console.log(consultaCEPConvetida);
+        return consultaCEPConvetida;
     } catch (erro) {
         console.log(erro)
     }
 }
 
-
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
